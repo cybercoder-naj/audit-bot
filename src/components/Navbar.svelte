6a78cac9eaa087d1f8483c2fa61af17eed832a73
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-03-21 15:01:16 +0530

    Navbar completed

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #2784EF;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="./images/favicon.png" alt="Schoolhouse.world" width="30" height="24" class="d-inline-block align-text-top">
+      Schoolhouse.world Audit Page
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav"
+        aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="#">Some</a>
+        <a class="nav-link" href="#">Thing</a>
+      </div>
+    </div>
+  </div>
+</nav>
